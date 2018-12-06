2be86690cec319ea1de6e72fa9d5d4554f4bbefc
Alex McDaniel <amcdaniel186@gmail.com>
2018-12-06 11:52:29 -0500

    initupdate

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    return(
+
+
+
+
+
+      <div>
+            {if route === '/'}{
+              <Main />
+            }
+
+            {if route === 'about-us'}{
+              <AboutUsComponent />
+            }
+
+            {if route === 'staff'}{
+              <MeetOurStaffComponent />
+            }
+
+            {if route === 'contact'}{
+              <ContactUsComponent />
+            }
+
+
+
+
+
+
+
+
       </div>
-    );
+    )
   }
 }
 
